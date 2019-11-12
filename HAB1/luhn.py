2d510e83d1502fe0e00d2313161bed5e5d3aa82b
ravedave <dic15dju@student.lu.se>
2019-11-12 17:32:26 +0100

    possbile progress

--- HAB1/luhn.py
+++ HAB1/luhn.py
@@ -1,5 +1,3 @@
-from textwrap import wrap
-
 
 def luhn():
     f = open('testin.txt', "r")
@@ -9,12 +7,23 @@ def luhn():
     for line in lines:
         line = line[::-1]
         validate_string(line.strip())
-                
-def letter_slice(string, index):
-    return string[::index]
 
-def validate_string(string):
-    chars = letter_slice(string, 2)
+
+def string_digit_conversion(string):
+    return [int(digit) for digit in str(string)]
+
+
+def unknown_removal(card_number):
+    result = ''.join([i for i in card_number if i.isdigit()])
+    return result
+
+
+def validate_string(card_number):
+    digits = unknown_removal(card_number)
+    odd_digits = digits[-1::-2]
+    even_digits = digits[-2::-2]
+    
+    
     
 
 
