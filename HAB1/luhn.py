7b6896c07e7081529750878e5d402f307087c2f3
ravedave <dic15dju@student.lu.se>
2019-11-14 15:48:35 +0100

    fail

--- HAB1/luhn.py
+++ HAB1/luhn.py
@@ -1,12 +1,31 @@
 
 def luhn():
-    f = open('testin.txt', "r")
+    f = open('test.txt', "r")
     lines = f.readlines()
     f.close
 
     for line in lines:
-        line = line[::-1]
-        print(line)
+        line = line.strip()
+        reversed_lines = line[::-1]
+        odd_index = reversed_lines[1::2]
+        even_index = reversed_lines[::2]
 
+        odd_index = odd_index.replace("X", "0")
+        even_index = even_index.replace("X", "0")
+
+        odd_index = list(map(int, odd_index))
+        even_index = list(map(int, even_index))  
+
+        odd_sum = sum(odd_index)  
+
+        even_index = [x * 2 for x in even_index]
+
+        digit_sum = 0
+        for number in even_index:
+            digit_sum += sum(map(int, str(number)))
+        
+        result = (10 - ((digit_sum + odd_sum) % 10))
+        print(result)
+        
 luhn()
 
