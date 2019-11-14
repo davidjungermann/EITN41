5440c4453b920dd82b91e08b47ffd00fff11007f
ravedave <dic15dju@student.lu.se>
2019-11-14 13:42:50 +0100

    removed

--- HAB1/luhn.py
+++ HAB1/luhn.py
@@ -6,33 +6,7 @@ def luhn():
 
     for line in lines:
         line = line[::-1]
-        validate_string(line.strip())
-
-
-def string_digit_conversion(string):
-    return [int(digit) for digit in str(string)]
-
-
-def unknown_removal(card_number):
-    result = ''.join([i for i in card_number if i.isdigit()])
-    return result
-
-
-def validate_string(card_number):
-    digits = unknown_removal(card_number)
-    odd_digits = digits[-1::-2]
-    even_digits = digits[-2::-2]
-    
-    check = 0
-    check += sum(string_digit_conversion(odd_digits))
-
-    for digit in even_digits: 
-        check += sum(string_digit_conversion(digit * 2)) 
-    print(check % 10)
-    return check % 10
-    
-    
-    
-
+        print(line)
 
 luhn()
+
