a17ed2aef9e19a26d84c09495ba0fb701fb59b9a
ravedave <dic15dju@student.lu.se>
2019-11-16 11:39:05 +0100

    B1 finished

--- HAB1/luhn.py
+++ HAB1/luhn.py
@@ -1,31 +1,60 @@
+def main():
+    lines = read_cards()
+    for line in lines:
+        digit_calc(line)
 
-def luhn():
-    f = open('test.txt', "r")
+
+def digit_calc(number):
+    number = reverse_number(number)
+    x_index = index_of_x(number)
+    calc_mod = (sum_calculation(number) % 10)
+
+    value = (10 - calc_mod) % 10
+    if (x_index) % 2 != 0:
+        if value % 2 != 0:
+            value += 9
+        value /= 2
+    print(str(int(value)), end="")
+    return str(int(value))
+
+
+def read_cards():
+    f = open('testin.txt', "r")
     lines = f.readlines()
     f.close
+    return lines
 
-    for line in lines:
-        line = line.strip()
-        reversed_lines = line[::-1]
-        odd_index = reversed_lines[1::2]
-        even_index = reversed_lines[::2]
 
-        odd_index = odd_index.replace("X", "0")
-        even_index = even_index.replace("X", "0")
+def sum_calculation(number):
+    sum = 0
+    number = number.replace("X", "0")
+    number = list(map(int, number))
 
-        odd_index = list(map(int, odd_index))
-        even_index = list(map(int, even_index))  
+    for i in range(len(number)):
+        digit = number[i]
+        if(i) % 2 != 0:
+            digit *= 2
+            if digit >= 10:
+                digit = sum_digits(digit)
+        sum += digit
+    return sum
 
-        odd_sum = sum(odd_index)  
 
-        even_index = [x * 2 for x in even_index]
+def index_of_x(number):
+    return number.find("X")
 
-        digit_sum = 0
-        for number in even_index:
-            digit_sum += sum(map(int, str(number)))
-        
-        result = (10 - ((digit_sum + odd_sum) % 10))
-        print(result)
-        
-luhn()
+
+def reverse_number(number):
+    number = number.strip()
+    number = number[::-1]
+    return number
+
+def sum_digits(n):
+    s = 0
+    while n:
+        s += n % 10
+        n //= 10
+    return s
+
+main()
 
