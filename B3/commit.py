689ac8311f6919645a1b347759436bcd8ab80e0d
ravedave <dic15dju@student.lu.se>
2019-12-04 18:51:36 +0100

    almost working

--- B3/commit.py
+++ B3/commit.py
@@ -3,56 +3,61 @@ from random import randint
 
 
 def create_commit(v, k, X):
-    sha1 = hashlib.sha1()
-    m = (bin(v)[2:] + k).encode()
-    sha1.update(m)
-    # Since we lose two bits after removing "0b" from bit strings.
-    return sha1.hexdigest()[2: X + 2]
+    m = str(bin(v)[2:] + bin(k)[2:])
+    hash_object = hashlib.sha1(m.encode())
+    hash_value = hash_object.hexdigest()
+    return hash_value[2: X + 2]
 
 
-# Generate 16-bit random string as k.
-def create_k(length):
-    k = ''
-    for i in range(length):
-        k += str(randint(0, 1))
-    return k
+def create_k():
+    values_of_k = []
 
-# Code from https://www.geeksforgeeks.org/python-intersection-two-lists/
-def intersection(left, right):
-    lst3 = [value for value in left if value in right]
-    print(len(lst3))
-    return len(lst3)
+    for i in range(1, 10):
+        values_of_k.append(i)
+    return values_of_k
 
 
-def construct_left_column(X):
+def construct_left_column(values, X):
     left_column = []
 
-    for i in range(2**15, 2**16): 
-        left_column.append(create_commit(0, create_k(16), X))
+    for k in values:
+        left_column.append(create_commit(0, k, X))
     return left_column
 
 
-def construct_right_column(X):
+def construct_right_column(values, X):
     right_column = []
 
-    for i in range(2**15, 2**16):
-        right_column.append(create_commit(1, create_k(16), X))
+    for k in values:
+        right_column.append(create_commit(1, k, X))
+
     return right_column
+
+# Code from https://www.geeksforgeeks.org/python-intersection-two-lists/
+
+
+def intersection(left, right):
+    res = [value for value in left if value in right]
+    print(res)
+    return len(res)
 
 
 def find_intersections(X):
-    intersections_left = []
-    intersections_right = []
 
-    left_column = construct_left_column(X)
-    right_column = construct_right_column(X)
+    left_column = construct_left_column(create_k(), X)
+    right_column = construct_right_column(create_k(), X)
 
-    for i in range(1, X):
-        for left, right in zip(left_column, right_column):
-            intersections_left.append(left)
-            intersections_right.append(right)
-            print("Hej")
-        print("Nbr of intersections found: " +
-          str(intersection(intersections_left, intersections_right)))
+    for trunc_value in range(1, X):
+        possible_intersections_left = []
+        possible_intersections_right = []
+
+        for left in left_column:
+            possible_intersections_left.append(left)
+
+        for right in right_column:
+            possible_intersections_right.append(right)
         
+        print("Nbr of intersections: " + str(intersection(possible_intersections_left,
+                                                          possible_intersections_right)) + " for X-value = " + str(trunc_value))
+
 find_intersections(10)
