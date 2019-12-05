25cf4530e4597d79bc342a64a14bf2d4df4ab68c
unknown <bas15jpe@student.lu.se>
2019-12-05 16:15:57 +0100

    hej

--- B3/B2/Shamir.py
+++ B3/B2/Shamir.py
@@ -8,17 +8,28 @@ def main():
     incoming_shares = []
     outgoing_shares = []
     polynom = []
+    
 
     for i in range(n):
         if i  + 1 !=your_index:
-           outgoing_shares.append(input("Input the value of your polynome for participan " + str(i + 1) + ": "))
+           outgoing_shares.append(int(input("Input the value of your polynome for participan " + str(i + 1) + ": ")))
     
     for i in range(n):
         if i + 1 !=your_index:
-           outgoing_shares.append(input("Input the recieved value from participan " + str(i + 1) + ": "))
+           outgoing_shares.append(int(input("Input the recieved value from participan " + str(i + 1) + ": ")))
     
     print("Your polynom inputs: ")
     for i in range(t):
-        input("Input the coefficient for x" + "^" + str(i) + ": ")
+        polynom.append(int(input("Input the coefficient for x" + "^" + str(i) + ": ")))
+    known_points = {your_index: calculate_point(outgoing_shares, polynom, your_index)}
+    print(known_points)
+
+def calculate_point(outgoing_shares, polynom, your_index):
+    point = 0
+    for i in range(len(polynom)):
+        point = point + polynom[i] * (your_index ** i)
+        print(str(polynom[i]) + " * " + str(your_index) + " ** " + str(i))
+
+    return(point)
 
 main()
