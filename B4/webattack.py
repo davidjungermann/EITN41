dbcf9da80c734ea9ccb30976dc7f6d3e5be5420a
ravedave <dic15dju@student.lu.se>
2019-12-12 01:03:20 +0100

    not working

--- B4/webattack.py
+++ B4/webattack.py
@@ -11,12 +11,14 @@ import requests
 import time
 import urllib3
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) # Turns off annoying warnings about certificates. 
+# Turns off annoying warnings about certificates.
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def http_request_time(URL, params):
     # Disables SSL verification since we don't have a valid certificate.
     r = requests.get(URL, params, verify=False)
+    print(r.elapsed.total_seconds())
     return r.elapsed.total_seconds()
 
 
@@ -34,18 +36,23 @@ def generate_signature(name, grade):
     possible_chars = generate_chars()
     params = {'name': name, 'grade': grade, 'signature': signature}
     for i in range(20):  # Signature is 20 chars long.
-        max_time = 0
+        max_time = 0.0
+        subject_char = ''
         for char in possible_chars:
             temp = signature + char
-            params[signature] = temp
+            params['signature'] = temp
 
-            current_time = http_request_time(URL, params)
+            current_time = min([http_request_time(URL, params) for i in range(1)])
 
             if current_time > max_time:
                 max_time = current_time
-                signature += char
-        print("Working...")
-    print(len(signature))
+                subject_char = char
+
+        signature += subject_char
+        print(signature)
     print(signature)
 
+
 generate_signature("Kalle", "5")
+
+print(len("03f058c3b89ec744dc2c"))
