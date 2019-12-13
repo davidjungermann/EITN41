e6aa457873601c947e3ea1c1fc86b1c403b4ffd3
ravedave <dic15dju@student.lu.se>
2019-12-13 14:58:16 +0100

    hej

--- B4/oaep.py
+++ B4/oaep.py
@@ -16,33 +16,83 @@
 from math import ceil
 from hashlib import sha1
 
+hLen = 20
+k = 128
+
+
 def MGF1(mgfSeed, maskLen):
-    hLen = 20
     T = ""
-    if maskLen > 2 ^ 32:
+    if maskLen > 2 ** 32:
         print("Mask too long")
         return None
 
     for i in range(ceil(maskLen/hLen)):
         C = I2OSP(i, 4)
-        concat = (mgfSeed + C)
+        concat = str(mgfSeed) + C
         digest = sha1(bytearray.fromhex(concat)).hexdigest()
         T = T + digest
-    print(T[:2 * maskLen])
-    return T[:2 * maskLen ]
+    #print(T[:2 * maskLen])
+    return T[:2 * maskLen]
 
-def I2OSP(x, xLen):      
-    if x >= 256 ** xLen:    
+
+def I2OSP(x, xLen):
+    if x >= 256 ** xLen:
         print("Integer too large")
         return None
     base256 = ""
-    for i in range(xLen - 1, -1 ,-1):
+    for i in range(xLen - 1, -1, -1):
         byte = int(x / 256 ** i)
         x -= byte * 256 ** i
         byte = hex(byte)
         byte = byte[2:]
         if len(byte) == 1:
             byte = "0" + byte
-        base256 += byte 
+        base256 += byte
     return base256
-MGF1("0123456789abcdef", 30) 
+
+
+def OAEP_encode(m, seed, L=""):
+    if len(L) >= (2 ** 64 - 1):
+        print("label too long")
+        return
+
+    if len(m) > k - 2 * hLen - 2:
+        print("message too long")
+        return
+
+    lHash = sha1(bytearray(L.encode())).hexdigest()
+    PS = "".zfill(2 * k - len(m) - 4 * hLen - 4)
+    DB = lHash + PS + "01" + m
+
+    dbMask = MGF1(seed, k - hLen - 1)
+    maskedDB = hex(int(DB, 16) ^ int(dbMask, 16))[2:]
+
+    seedMask = MGF1(maskedDB, hLen)
+    maskedSeed = hex(int(seed, 16) ^ int(seedMask, 16))[2:]
+
+    EM = ("00" + maskedSeed + maskedDB).zfill(2 * k)
+    print(EM)
+    return EM
+
+def OAEP_decode(EM, L= ""):
+    if len(L) >= (2 ** 64 - 1):
+        print("label too long")
+        return
+
+    lHash = sha1(bytearray(L.encode())).hexdigest()
+    Y = EM[2:]
+    maskedDB = EM[2 * hLen + 2]
+    maskedSeed = EM[2: hLen * 2 + 2]
+    
+    seedMask = MGF1(maskedDB, hLen)
+    seed = hex(int(maskedSeed, 16) ^ int(seedMask, 16))[2:]
+
+    dbMask = MGF1(seed, k - hLen - 1)
+    DB = hex(int(maskedDB, 16) ^ int(dbMask, 16))[2:]
+    
+    
+
+
+
+OAEP_decode("Hej")
+
