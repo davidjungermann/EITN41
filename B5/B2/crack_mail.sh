a65a0d9e6954746bf258a64a1bb3635a9570f8bc
ravedave <dic15dju@student.lu.se>
2019-12-17 11:21:28 +0100

    begin b3

--- B5/B2/crack_mail.sh
+++
@@ -1,5 +0,0 @@
-# Verify email
-openssl cms -decrypt -inkey keyreceiver.pem -in mail1.msg | openssl cms -digest_verify
-
-# See email subject
-openssl cms -decrypt -inkey keyreceiver.pem -in mail1.msg
